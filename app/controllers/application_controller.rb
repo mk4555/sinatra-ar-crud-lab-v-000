31672bac486f55181334c4e2bffc2cd6c1cbccb6
Min Kim <mk4555@nyu.edu>
2018-01-16 05:26:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
   delete '/posts/:id/delete' do
     @post = Post.find_by_id(params[:id])
     @post.delete
-    redirect to '/:id/delete'
+    redirect to '/params[:id]/delete'
   end
 
   get '/:id/delete' do
